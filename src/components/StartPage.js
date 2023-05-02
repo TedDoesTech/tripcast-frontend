25df4379759a3dfec25c3a6431d6cc91299b567a
Chris Edwards <chrisedwards96@outlook.com>
2023-05-02 14:21:45 +0100

    Added in functionality to the search forms/ button

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/startPage.css";
 
 const StartPage = () => {
+  const [startPoint, SetStartPoint] = useState("");
+  const [destinationPoint, setDestinationPoint] = useState("");
+  const [travelTime, setTravelTime] = useState(null);
+
+  const getTravelTime = async (startPoint, destinationPoint) => {
+    console.log(startPoint, destinationPoint);
+  };
+
   return (
-    <div className="container">
+    <div className="start-page container">
       <h1>Your Journey</h1>
       <p>Start a Journey to match your cast!</p>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <label htmlFor="start-point">Start Point:</label>
         <input
           type="text"
           id="start-point"
           name="start-point"
           placeholder="Enter starting location"
+          onChange={(e) => SetStartPoint(e.target.value)}
         />
-        <br />
+
         <label htmlFor="destination">Destination:</label>
         <input
           type="text"
           id="destination"
           name="destination"
           placeholder="Enter destination"
+          onChange={(e) => setDestinationPoint(e.target.value)}
         />
-        <br />
+        <button onClick={() => getTravelTime(startPoint, destinationPoint)}>
+          Cast
+        </button>
       </form>
     </div>
   );
